9dc45616263555e506bf0b785e15ef1dd4a6813e
steve <jeaniedaddy@gmail.com>
2018-08-16 14:04:20 -0400

    security framework added

--- src/main/java/com/philly/asset/controllers/HomeController.java
+++ src/main/java/com/philly/asset/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.philly.asset.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +15,15 @@ public class HomeController {
     @RequestMapping("/about")
     public String about(){
         return "home/about";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "home/login";
+    }
+
+    @PostMapping("/login")
+    public String loggnedIn(){
+        return "redirect:/mobilestatus";
     }
 }
